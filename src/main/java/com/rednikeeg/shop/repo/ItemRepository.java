c9cd8d9e503cd344068d0932e8d52108c276b95e
rednikeeg <maks.schoolforvv@gmail.com>
2022-08-24 13:24:23 +0300

    categories: repository layer is implemented

---
+++ src/main/java/com/rednikeeg/shop/repo/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.rednikeeg.shop.repo;
+
+import com.rednikeeg.shop.domain.Item;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemRepository extends AbstractCrudRepository<Item> {
+}
