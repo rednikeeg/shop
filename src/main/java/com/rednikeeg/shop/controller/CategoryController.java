22a467c1486c1eda0feb2f7558ac828e0c7d0400
rednikeeg <maks.schoolforvv@gmail.com>
2022-10-11 01:35:23 +0300

    categories: adjusted controllers

--- src/main/java/com/rednikeeg/shop/controller/CategoryController.java
+++ src/main/java/com/rednikeeg/shop/controller/CategoryController.java
@@ -3,31 +3,29 @@ package com.rednikeeg.shop.controller;
 import com.rednikeeg.shop.dto.CategoryDto;
 import com.rednikeeg.shop.service.CategoryService;
 import com.rednikeeg.shop.util.CategoryTransformer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 @RequestMapping(path = "/api/categories", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CategoryController {
-
-    @Autowired
-    private CategoryService categoryService;
-    @Autowired
-    private CategoryTransformer categoryTransformer;
-
-    @GetMapping
-    @ResponseBody
-    public List<CategoryDto> getAll() {
-        return categoryService.getAll()
-                .stream()
-                .map(categoryTransformer::toDto)
-                .collect(Collectors.toList());
-    }
+    private final CategoryService categoryService;
+    private final CategoryTransformer categoryTransformer;
 
     @PostMapping
     @ResponseBody
@@ -36,7 +34,16 @@ public class CategoryController {
                 .map(categoryTransformer::toEntity)
                 .map(categoryService::save)
                 .map(categoryTransformer::toDto)
-                .orElseThrow(() -> new IllegalArgumentException("Category " + category + "cannot be saved."));
+                .orElseThrow(() -> new IllegalArgumentException("Category " + category + " cannot be saved."));
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<CategoryDto> getAll() {
+        return categoryService.getAll()
+                .stream()
+                .map(categoryTransformer::toDto)
+                .collect(Collectors.toList());
     }
 
     @GetMapping(path = "/:{id}")
@@ -48,7 +55,7 @@ public class CategoryController {
                 .orElseThrow(() -> new IllegalArgumentException("Category with id " + id + " cannot be found."));
     }
 
-    @PutMapping(path = ":{id}")
+    @PutMapping(path = "/:{id}")
     @ResponseBody
     public CategoryDto update(@PathVariable Long id, @RequestBody CategoryDto category) {
         return Optional.of(category)
@@ -60,7 +67,7 @@ public class CategoryController {
 
     @DeleteMapping(path = "/:{id}")
     @ResponseBody
-    public CategoryDto deleteById(@PathVariable Long id) {
+    public CategoryDto delete(@PathVariable Long id) {
         return Optional.of(id)
                 .map(categoryService::deleteById)
                 .map(categoryTransformer::toDto)
